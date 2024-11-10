4886ea65beee93158204ca89c6babbe5379ffea3
Sneha Patil <9patilsneha@gmail.com>
2024-11-10 12:19:48 +0000

    updated

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 
-# Rebuild your images
- docker build -t messaging-service ./messagingservice
- docker build -t user-management-service ./usermanagement
+mvn -f ./datapackage/pom.xml install
+
+cp ./datapackage/target/datapackage-1.0-SNAPSHOT.jar ./messagingservice
+cp ./datapackage/target/datapackage-1.0-SNAPSHOT.jar ./usermanagement
+
+docker build -t user-management-service ./usermanagement
+
+docker build -t messaging-service ./messagingservice
+
+ 
+
+
+
